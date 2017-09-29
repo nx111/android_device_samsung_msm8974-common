c45714a2057f7c02ed52248c15177600e17b710e
Kevin F. Haggerty <haggertk@lineageos.org>
2017-09-28 21:15:20 -0600

    msm8974-common: HIDL: Build DRM HAL(s)
    Change-Id: I925afdd40577bcdb0082844b74ed62d44f7649bc

--- msm8974.mk
+++ msm8974.mk
@@ -76,6 +76,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
