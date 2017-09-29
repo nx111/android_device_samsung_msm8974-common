971af90063a89242bce49391dafb7d339a50bb60
Kevin F. Haggerty <haggertk@lineageos.org>
2017-09-28 21:20:55 -0600

    msm8974-common: HIDL: Build USB service
    Change-Id: Idd8f6a30d810683cd43cd22c4aa88c58e2409eb5

--- msm8974.mk
+++ msm8974.mk
@@ -133,5 +133,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # common msm8974
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
